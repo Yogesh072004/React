84e86baaa83076ac92e8ae21100b7ebf24380e2b
Yogesh072004 <yp901981@gmail.com>
2024-08-15 17:27:26 +0530

    added class component

--- 03.class Component/src/App.jsx
+++ 03.class Component/src/App.jsx
@@ -1,35 +1,35 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+// 1st way
+// import React from 'react'
+// class App extends React.Component{
+//   render(){
+//     return(
+//       <h1>hi yogesh</h1>
+//     );
+//   }
+// }
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+// 2nd way
+import React ,{Component}from 'react'
+class App extends Component{
+  render(){
+    return(
+      <>
+      <h1>hi yogesh</h1>
+      <App2/>
+      </>
+    );
+  }
 }
 
-export default App
+export   function App2(){
+return(
+<h2>Function Component</h2>
+);
+}
+export default App;
+
+
--- 03.class Component/src/main.jsx
+++ 03.class Component/src/main.jsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import App ,{App2}from './App.jsx'
+// import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <App2/>
   </StrictMode>,
 )
